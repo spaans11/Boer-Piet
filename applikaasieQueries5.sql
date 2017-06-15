3f24644c6d8bd618201c62d9e8382db3db24040d
spaans11 <lennartspaans@hotmail.com>
2017-06-15 09:44:17 +0200

    Delete applikaasieQueries5.sql

--- applikaasieQueries5.sql
+++
@@ -1,7 +0,0 @@
-select klant.id, artikel_id, naam, aantal, prijs, totaalprijs
-from klant
-join bestelling join bestel_regel join artikel
-on klant.id = bestelling.klant_id and bestelling.id = bestel_regel.id and bestel_regel.artikel_id = artikel.id
-where klant.id =1
-
-ORDER BY artikel_id
